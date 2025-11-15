40868cfc061f92512fd2146ebfd60f39699e68c7
victor-lee-123 <victorlee100@hotmail.com>
2025-11-15 18:35:51 +0800

    Fixed

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# energy.py
+import cv2
+
+img = cv2.imread("images/test.jpg")
+
+if img is None:
+    print("Failed to load image!")
+else:
+    print("Image loaded successfully:", img.shape)
+    cv2.imshow("Test Image", img)
+    cv2.waitKey(0)
